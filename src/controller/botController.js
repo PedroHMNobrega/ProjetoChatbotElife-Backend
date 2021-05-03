1a47a3841382204bc2c073c9eae206f89133c15f
PedroHMNobrega <pedrohmnobrega@gmail.com>
2021-05-03 17:28:28 -0300

    Enviando noticias do banco de dados para o bot

--- src/controller/botController.js
+++ src/controller/botController.js
@@ -1,40 +1,51 @@
+const newsRepository = require("../service/repositories/NewsRepository.js");
+
 module.exports = {
     webhook: async (req, res) => {
-        var intentName = req.body.queryResult.intent.displayName;
-        res.json({"fulfillmentMessages" : [
-            {
-                "payload": {
-                    "facebook": {
-                        "attachment": {
-                            "payload": {
-                                "template_type": "generic",
-                                "elements": [
-                                    {
-                                        "buttons": [
-                                            {
-                                                "title": "Acessar",
-                                                "url": "https://globoesporte.globo.com/futebol/futebol-internacional/liga-dos-campeoes/noticia/mbappe-ainda-manca-mas-viaja-com-psg-para-enfrentar-manchester-city.ghtml",
-                                                "type": "web_url"
-                                            }
-                                        ],
-                                        "image_url": "https://i1.wp.com/blogsucessoempresarial.com/wp-content/uploads/2018/07/copa-do-mundo-mbape.jpg?fit=1200%2C641&ssl=1",
-                                        "default_action": {
-                                            "webview_height_ratio": "tall",
-                                            "messenger_extensions": false,
-                                            "type": "web_url",
-                                            "url": "https://globoesporte.globo.com/futebol/futebol-internacional/liga-dos-campeoes/noticia/mbappe-ainda-manca-mas-viaja-com-psg-para-enfrentar-manchester-city.ghtml"
-                                        },
-                                        "subtitle": "Descricao Noticia",
-                                        "title": "Titulo Noticia Legal"
-                                    }
-                                ]
-                            },
-                            "type": "template"
+        const intentName = req.body.queryResult.intent.displayName;
+
+        if(intentName === 'mostrar-categorias') {
+            const categoria = req.body.queryResult.parameters.categoria;
+            const news = await newsRepository.listTenByCategory(categoria);
+
+            const elements = [];
+            news.forEach(n => {
+               elements.push({
+                   "title": n.title,
+                   "subtitle": n.description,
+                   "image_url": n.imgUrl,
+                   "default_action": {
+                       "webview_height_ratio": "tall",
+                       "messenger_extensions": false,
+                       "type": "web_url",
+                       "url": n.url
+                   },
+                   "buttons": [
+                       {
+                           "title": "Acessar",
+                           "url": n.url,
+                           "type": "web_url"
+                       }
+                   ],
+               })
+            });
+
+            res.json({"fulfillmentMessages" : [
+                {
+                    "payload": {
+                        "facebook": {
+                            "attachment": {
+                                "payload": {
+                                    "template_type": "generic",
+                                    "elements": elements
+                                },
+                                "type": "template"
+                            }
                         }
-                    }
-                },
-                "platform": "FACEBOOK"
-            }
-        ]});
+                    },
+                    "platform": "FACEBOOK"
+                }
+            ]});
+        }
     },
 }
