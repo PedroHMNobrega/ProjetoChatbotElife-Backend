039050fb127ec1da39bcff8668bf126ac663cf41
PedroHMNobrega <pedrohmnobrega@gmail.com>
2021-05-04 13:34:46 -0300

    Adicionando mensagem quando nao tiver noticias

--- src/controller/botController.js
+++ src/controller/botController.js
@@ -7,49 +7,73 @@ module.exports = {
         if(intentName === 'mostrar-noticias') {
             const categoria = req.body.queryResult.parameters.categoria;
             const news = await newsRepository.listTenByCategory(categoria);
+            let fulfilmentMessages;
 
             if(news.length === 0) {
-                res.end();
+                fulfilmentMessages =  [
+                    {
+                        "text": {
+                            "text": [
+                                "Desculpa, não encontrei nenhuma notícia nessa categoria"
+                            ]
+                        },
+                        "platform": "FACEBOOK"
+                    },
+                    {
+                        "quickReplies": {
+                            "title": "Escolha uma categoria:",
+                            "quickReplies": [
+                                "Esportes",
+                                "Política",
+                                "Entretenimento",
+                                "Famosos"
+                            ]
+                        },
+                        "platform": "FACEBOOK"
+                    },
+                ]
+            } else {
+                const elements = [];
+                news.forEach(n => {
+                   elements.push({
+                       "title": n.title,
+                       "subtitle": n.description,
+                       "image_url": n.imgUrl,
+                       "default_action": {
+                           "webview_height_ratio": "tall",
+                           "messenger_extensions": false,
+                           "type": "web_url",
+                           "url": n.url
+                       },
+                       "buttons": [
+                           {
+                               "title": "Acessar",
+                               "url": n.url,
+                               "type": "web_url"
+                           }
+                       ],
+                   })
+                });
+
+                fulfilmentMessages = [
+                    {
+                        "payload": {
+                            "facebook": {
+                                "attachment": {
+                                    "payload": {
+                                        "template_type": "generic",
+                                        "elements": elements
+                                    },
+                                    "type": "template"
+                                }
+                            }
+                        },
+                        "platform": "FACEBOOK"
+                    }
+                ]
             }
 
-            const elements = [];
-            news.forEach(n => {
-               elements.push({
-                   "title": n.title,
-                   "subtitle": n.description,
-                   "image_url": n.imgUrl,
-                   "default_action": {
-                       "webview_height_ratio": "tall",
-                       "messenger_extensions": false,
-                       "type": "web_url",
-                       "url": n.url
-                   },
-                   "buttons": [
-                       {
-                           "title": "Acessar",
-                           "url": n.url,
-                           "type": "web_url"
-                       }
-                   ],
-               })
-            });
-
-            res.json({"fulfillmentMessages" : [
-                {
-                    "payload": {
-                        "facebook": {
-                            "attachment": {
-                                "payload": {
-                                    "template_type": "generic",
-                                    "elements": elements
-                                },
-                                "type": "template"
-                            }
-                        }
-                    },
-                    "platform": "FACEBOOK"
-                }
-            ]});
+            res.json({"fulfillmentMessages" : fulfilmentMessages});
         }
     },
 }
